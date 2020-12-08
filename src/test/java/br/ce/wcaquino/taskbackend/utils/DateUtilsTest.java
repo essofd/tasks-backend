e69e86e93f0229284d64314aa34e5c8e1fbba750
essofd <essofd@gmail.com>
2020-12-08 12:09:53 -0300

    Testando os testes

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate data=LocalDate.of(2030, 01, 01);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(data));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(data));
 	}
 
 	@Test
